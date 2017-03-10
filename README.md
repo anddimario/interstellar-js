5f5027db191ab5fffeb70cfab8018a43511f9a69
Andrea Di Mario <anddimario@gmail.com>
2017-03-10 12:25:39 +0100

    chain of functions (middleware)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # INUTILE
-Http server that exec cmd and use redis to host/url mapping, started to test [gravity-lang](https://marcobambini.github.io/gravity/) as server language.
+Http server that exec cmd and use redis to host/url mapping, started to test [gravity-lang](https://marcobambini.github.io/gravity/) as server language. You can use a middleware feature to create a chain of functions
 
 ### Requirements
 Node.JS and redis
@@ -31,6 +31,30 @@ Example:
 `hset vhost:localhost:3000:/ file mytest.gravity`     
 - Test with curl    
 `curl http://localhost:3000/`
+
+### Gravity advanced example (use middleware)
+Extend the basic example with middleware usage   
+- Create a new gravity file in the same path of mytest.gravity, we can call it mymid.gravity, with this basic code:
+```
+func main () {
+  return "ok" 
+}
+```
+- Then we can add a route for this, with:     
+`hset vhost:localhost:3000:/ciao path /home/myuser/gravity`     
+`hset vhost:localhost:3000:/ciao file mymid.gravity,mytest.gravity`     
+*Note* The function order is important, and use commas to separate them
+- Test with curl    
+`curl http://localhost:3000/ciao`     
+- Try now to modify mymid.gravity as:     
+```
+func main () {
+  return false 
+}
+```
+- Test again with curl    
+`curl http://localhost:3000/ciao`     
+- See that if the middleware return true, the exec was blocked
 
 ### License
 MIT
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 /*
  * INUTILE
  * http server
@@ -6,16 +7,17 @@
 // Requirements
 require('dotenv').config()
 
-const http = require('http')  
+const http = require('http')
 const exec = require('child_process').exec
 const port = process.env.PORT
 const execCmd = process.env.EXEC_CMD
 const redis = require('redis')
 const redisClient = redis.createClient()
+const async = require('async')
 
 // Docs: https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
 
-const requestHandler = (request, response) => {  
+const requestHandler = (request, response) => {
   // Get path info from redis
   redisClient.hgetall(`vhost:${request.headers.host}:${request.url}`, (errRedis, resVhost) => {
     if (errRedis) {
@@ -25,16 +27,52 @@ const requestHandler = (request, response) => {
       response.statusCode = 404;
       response.end('not found')
     } else {
-      // Exec the command and response
-      exec(`cd ${resVhost.path} && ${execCmd} ${resVhost.file}`, {encoding: 'utf8'}, (err, stdout, stderr) => {
+      // Split the file value
+      const files = resVhost.file.split(',')
+      let tasks = []
+      // Create task for waterfall, based on files
+      for (let i = 0; i < files.length; i++) {
+        // First task
+        if (i === 0) {
+          tasks.push((callback) => {
+            // Exec the command and response
+            exec(`cd ${resVhost.path} && ${execCmd} ${files[i]}`, { encoding: 'utf8' }, (err, stdout, stderr) => {
+              if (err) {
+                callback(stderr)
+              } else {
+                callback(null, stdout)
+              }
+
+            })
+          })
+        } else { // normal task
+          tasks.push((previous, callback) => {
+            // Check if previous return is false
+            // Gravity note: return false give on stdout: RESULT: (BOOL) false
+            if (previous.indexOf('false') !== -1) {
+              callback('Middleware error')
+            } else {
+              // Exec the command and response
+              exec(`cd ${resVhost.path} && ${execCmd} ${files[i]}`, { encoding: 'utf8' }, (err, stdout, stderr) => {
+                if (err) {
+                  callback(stderr)
+                } else {
+                  callback(null, stdout)
+                }
+
+              })
+            }
+          })
+        }
+      }
+      async.waterfall(tasks, (err, results) => {
         if (err) {
           response.statusCode = 500;
-          response.end(stderr)
+          response.end(err)
         } else {
-          response.end(stdout)
+          response.end(results)
         }
-
-      });
+      })
     }
 
   });
@@ -42,7 +80,7 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {  
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
